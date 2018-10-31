63bb43b1aa6c7cd05bca4f8ad291c50582cb7a97
jyp <jyp@192.168.0.6>
2018-11-01 07:57:48 +0800

    code

--- func/BelowEstimate.py
+++ func/BelowEstimate.py
@@ -1,4 +1,14 @@
 # encoding: utf-8
 import numpy as np
+import math
 
 print('测试')
+def distanceE(r,rh,gamaV,RL,t,th):
+    rA = 2*r+2*rh+2;
+    print(rA)
+    gamaA = gamaV*rA
+    RLA = -2*(math.log(RL,math.e))
+    tK = gamaA/RLA
+    tA = tK*t-th 
+    return tA;
+

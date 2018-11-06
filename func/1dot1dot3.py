eb71277ed9852de21c5d6affb5962f343017ce9e
duocolours <yangping_sx@163.com>
2018-11-06 16:45:24 +0800

    已知t分布自由度，概率，求分位点

--- func/1dot1dot3.py
+++ func/1dot1dot3.py
@@ -6,19 +6,26 @@ Created on 2018年11月5日
 import math
 from scipy.stats import norm
 from scipy.stats import nct
-from scipy.stats import t
 
 #已知正态分布密度函数面积，求分位点，参数只需要概率值
 def getNormVal(r):
     return (norm.ppf(r))
 
-#非中心t分布，参数 概率值，自由度，非中心参数
-print((nct.ppf(0.7,9,5.201630523210968))/math.sqrt(10))
-print('###',t.ppf(0.8,9))
-#print (t.ppf(0.7,9)*np.sqrt(10)+5.2)
+#可靠度下限算法，根据样本量n,求剩余强度系数K和寿命均值
+def distanceETCalTime(RL,gama,n,t,s):
+    #根据正态分布概率值，求分位点；omegal
+    #'%.4f'控制小数的精度，标准的四舍五入，round()函数逢5如果前面是偶数就舍掉了
+    omegal = float('%.4f' % (norm.ppf(RL)))
+    print(omegal)
+    #非中心t分布，参数: 概率值，自由度，非中心参数
+    K = float('%.4f' % (nct.ppf(gama,n-1,omegal*math.sqrt(n))/math.sqrt(n)))
+    print(K)
+    xMean =  t + (K * s)
+    return(xMean)
 
-test = getNormVal(0.95)
-#print('****',test)
-print(pow(10,0.5)*1.6449)
-print(math.sqrt(10))
+#可靠度下限算法，根据剩余强度系数K，求样本量和寿命均值
+def distanceETCalNum(RL,gama,K,t,s):
+    omegal = float('%.4f' % (norm.ppf(RL)))
+    print (10/8)
 
+print(distanceETCalTime(0.95,0.7,10,1000,10));
